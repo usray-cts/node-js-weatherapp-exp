e6bfb29a0ae636e941206e45a472683691491777
usray07 <usray0706@gmail.com>
2022-06-10 18:08:17 +0530

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#m1')
+const message2 = document.querySelector('#m2')
+
+message1.textContent = ""
+message2.textContent = ""
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location = search.value 
+    message1.textContent = "Loading..."
+    message2.textContent = ""
+
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+        response.json().then((data)=>{
+            if (data.error){
+                message1.textContent = data.error
+            }
+            else{
+                message1.textContent = data.location
+                message2.textContent = data.weather
+            }
+        })
+    })
+
+    console.log('testing',location)
+    
+})
---
+++ src/app.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const request = require('request')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates')
+
+app.set('view engine', 'hbs')
+app.set('views',viewsPath)
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Uttam Sundar Ray'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
+
+app.get('/weather', (req, result) => {
+    if(!req.query.address){
+        return result.send({
+            error: "Please give a location to search weather info"
+        })
+    }
+    const place = req.query.address
+    geocode(place,(error,{lat,long,location}= {})=>{
+        if(error){
+            // console.log(error)
+            return result.send({
+                error: error
+            })
+        }
+        forecast(lat,long,(err,res)=>{
+            if(err){
+                // console.log(err)
+                return result.send({
+                    error:err
+                })
+            }
+            result.send({
+                location: location,
+                weather: res
+
+            })
+            // console.log(location,'Weather:')
+            // console.log(res)
+            // console.log()
+        })
+    }) 
+   
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
